e7541207dcb62a85cef63f5fb6c267378ca5375d
гк <@adamant.local>
2022-01-27 21:13:16 +0300

    function

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const numbersBtn = document.querySelectorAll ('.numbers'),
-      operatorsBtn = document.querySelectorAll ('.operators'),
-      resultBtn = document.querySelectorAll ('.result'),
-	   clearBtn = document. querySelectorAll ('.clear'),
-	   clearAllBtn = document. querySelectorAll ('.Allclear'),
-	   decimalBtn = document. querySelectorAll ('.decimal')
+const numbersBtn = document.querySelectorAll(".numbers"),
+  operatorsBtn = document.querySelectorAll(".operators"),
+  resultBtn = document.querySelectorAll(".result"),
+  clearBtn = document.querySelectorAll(".clear"),
+  clearAllBtn = document.querySelectorAll(".Allclear"),
+  decimalBtn = document.querySelectorAll(".decimal");
 
-		
-		console.log (numbersBtn);
-		console.log (operatorsBtn);
-		console.log (resultBtn);
-		console.log (clearBtn);
-		console.log (clearAllBtn);
-		console.log (decimalBtn); 
+let display = document.querySelector(".display");
 
+console.log(display);
+
+function numberPress(number) {
+  if (display.value === '0') {
+    display.value = number;
+  } else {
+    display.value += number;
+  }
+};
+
+
+numbersBtn.forEach((number) => {
+  number.addEventListener("click",(event) => {
+    numberPress(event.target.textContent);
+  });
+});
