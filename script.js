75606da7b9edbb09cf098dd6072c4c9845618591
ominchikova <ominchikova@mail.ru>
2022-01-27 18:52:11 +0200

    hover

---
+++ script.js
@@ -0,0 +1,15 @@
+const numbersBtn = document.querySelectorAll ('.numbers'),
+      operatorsBtn = document.querySelectorAll ('.operators'),
+      resultBtn = document.querySelectorAll ('.result'),
+	   clearBtn = document. querySelectorAll ('.clear'),
+	   clearAllBtn = document. querySelectorAll ('.Allclear'),
+	   decimalBtn = document. querySelectorAll ('.decimal')
+
+		
+		console.log (numbersBtn);
+		console.log (operatorsBtn);
+		console.log (resultBtn);
+		console.log (clearBtn);
+		console.log (clearAllBtn);
+		console.log (decimalBtn); 
+
